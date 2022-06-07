4590d7fd3657883867aef32619917f29a5958dd2
rebahama <wiri@hotmail.se>
2022-06-07 01:32:37 +0000

    add delete

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -42,6 +42,13 @@ def delete_category(category_id):
     db.session.commit()
     return redirect(url_for("categories"))
 
+@app.route("/delete_task/<int:task_id>")
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("home"))
+
 
 @app.route("/add_task", methods=["GET", "POST"])
 def add_task():
